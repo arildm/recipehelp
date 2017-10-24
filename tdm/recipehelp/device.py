69da8036e77a4fd603e6f2bc6b0b9fb261d95778
Arild Matsson <gusmatssar@dtlserv.flov.gu.se>
2017-10-24 15:23:10 +0200

    real list, go back;

--- tdm/recipehelp/device.py
+++ tdm/recipehelp/device.py
@@ -1,5 +1,10 @@
 from tdm.lib.device import DddDevice, EntityRecognizer, DeviceWHQuery, DeviceAction
 
+recipelist = ["Step one: preheat the oven to 180 degrees.",
+"Step two: Make the ragu. Heat the olive oil in a frying pan and fry the celery, carrots and onion for approximately 15 minutes. Add the beef and pork. Pour in red wine and stir well. Cook for 45 minutes.",
+"Step three: add the tomatoes and stock. Cook slowly for two hours. Season with salt and freshly ground black pepper.",
+"Step four: make the bechamel sauce. Place the milk in a saucepan, add nutmeg and bring to the boil. In a separate saucepan melt the butter and add the flour. Remove the milk from the heat and add a little to the flour mixture. .",
+"Step five: assemble lasagne. Blanch the pasta in salted boiling water for three minutes. Spoon a third of the bechamel sauce into the bottom of a lasagne dish in a layer, then place some of the pasta strips over the top. Follow this with some of the mozzarella and parmesan, and sprinkle over freshly ground black pepper. Follow with a layer of ragu. Repeat this step twice until all the ingredients have been used up. Dot knobs of butter over the surface and cook in the oven for 30 minutes"]
 
 
 class RecipehelpDevice(DddDevice):
@@ -7,28 +12,45 @@ class RecipehelpDevice(DddDevice):
 		self.reset()
 
 	def reset(self):
-		self._step = 0
+		self._step = 1
 
 	class go_to_step(DeviceWHQuery):
 		PARAMETERS = ["step_to_choose"]
 		def perform(self,step):
 			self.device._step = int(step[4:])
-			return ["chose a step"]
+			return ["chose step"+ str(self.device._step)]
 
 
 	class go_to_next(DeviceAction):
 		def perform(self):
-			self.device._step += 1
-			return True
+			if self.device._step <=2:
+				self.device._step += 1
+				return True
+			else:
+				return ["All the steps have been completed"]
+
+	class go_back(DeviceAction):
+		def perform(self):
+			if self.device._step >= 2 :
+				self.device._step -= 1
+				return True
+			else:
+				self.device._step = 1
+				return True
 
 	class say_step(DeviceWHQuery):
 		def perform(self):
 			if self.device._step == 1:
-				return [" Step one: Chop onions and garlic."]
+				return [recipelist[0]]
 			elif self.device._step == 2:
-				return ["other example"]
+				return [recipelist[1]]
 			elif self.device._step == 3:
-				return ["lol"]
+				return [recipelist[2]]
+			elif self.device._step == 4:
+				return [recipelist[3]]
+			elif self.device._step == 5:
+				return [recipelist[4]]
 			else:
-				return [str(self.device._step)]
+				return ["You can choose a step from one to five"]
 
+	# return [str(self.device._step)]
